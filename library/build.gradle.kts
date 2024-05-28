69a6a47b6b2bd4faae29dd952261e96fadddd9d7
ity <1257545592@qq.com>
2024-05-28 14:13:25 +0800

    feat:add maven publish

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,6 +1,10 @@
+import com.vanniktech.maven.publish.AndroidSingleVariantLibrary
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.vanniktech.maven.publish") version "0.28.0"
 }
 
 android {
@@ -41,4 +45,47 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    configure(
+        AndroidSingleVariantLibrary(
+            // the published variant
+            variant = "release",
+            // whether to publish a sources jar
+            sourcesJar = true,
+            // whether to publish a javadoc jar
+            publishJavadocJar = true,
+        )
+    )
+
+    coordinates("io.github.zhangwenxue", "android-common", "1.0.0-alpha1")
+
+    pom {
+        name.set("Android-Ble-Common-lib")
+        description.set("An Android Ble common library")
+        inceptionYear.set("2024")
+        url.set("https://github.com/zhangwenxue/ble-common/")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("zwx")
+                name.set("zhangwenxue")
+                url.set("https://github.com/zhangwenxue/")
+            }
+        }
+        scm {
+            url.set("https://github.com/zhangwenxue/ble-common/")
+            connection.set("scm:git:git://github.com/zhangwenxue/ble-common.git")
+            developerConnection.set("scm:git:ssh://git@github.com/zhangwenxue/ble-common.git")
+        }
+    }
+    signAllPublications()
 }
